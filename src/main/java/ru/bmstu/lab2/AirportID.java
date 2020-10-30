2e5374ae10bfde58fc3865ef94e02fac80e1bc3e
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 16:11:03 +0300

    Scripted auto-commit on change (2020-10-30 16:11:03) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportID.java
+++ src/main/java/ru/bmstu/lab2/AirportID.java
@@ -7,7 +7,7 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class AirportID implements WritableComparable<AirportID> {
-    private String
+    private 
     @Override
     public int compareTo(AirportID o) {
         return 0;
