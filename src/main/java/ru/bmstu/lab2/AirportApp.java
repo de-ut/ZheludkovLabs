82524b31f2ba20005ff916e11a0000fb271998c0
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 23:48:22 +0300

    Scripted auto-commit on change (2020-10-30 23:48:22) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportApp.java
+++ src/main/java/ru/bmstu/lab2/AirportApp.java
@@ -33,6 +33,6 @@ public class AirportApp {
         job.setOutputValueClass(Text.class);
 
         job.setNumReduceTasks(2);
-        System.exit(job.waitForCompletion());
+        System.exit(job.waitForCompletion(true) ? );
     }
 }
