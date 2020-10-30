bd8ed037df27f707aa2c491bca5f682380a33cd1
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 23:27:34 +0300

    Scripted auto-commit on change (2020-10-30 23:27:33) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportReducer.java
+++ src/main/java/ru/bmstu/lab2/AirportReducer.java
@@ -19,6 +19,8 @@ public class AirportReducer extends Reducer<AirportID, Text, Text, Text> {
         float max = 0;
         float avg = 0;
 
-        while(iter.hasNext())
+        while(iter.hasNext()){
+            
+        }
     }
 }
