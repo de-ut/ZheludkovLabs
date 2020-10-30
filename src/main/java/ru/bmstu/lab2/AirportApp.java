2a8f31770bca6dabc000ab9a73166ee84f9cc4f3
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 15:56:53 +0300

    Scripted auto-commit on change (2020-10-30 15:56:53) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportApp.java
+++ src/main/java/ru/bmstu/lab2/AirportApp.java
@@ -2,6 +2,8 @@ package ru.bmstu.lab2;
 
 public class AirportApp {
     public static void main(String[] args) throws Exception{
-        if(args.length != )
+        if(args.length != 3){
+            
+        }
     }
 }
