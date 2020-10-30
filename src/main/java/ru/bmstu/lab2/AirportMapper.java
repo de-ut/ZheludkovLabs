6db54c4efd1af4875eeac7d785ce631702f80933
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 23:55:51 +0300

    Scripted auto-commit on change (2020-10-30 23:55:51) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/lab2/AirportMapper.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 public class AirportMapper extends Mapper<LongWritable, Text, AirportID, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if(key.get() == 0) return;
         String[] fields = value.toString().replace("\"", "").split(",");
         context.write(new AirportID(fields[0], true), new Text(fields[1]));
     }
