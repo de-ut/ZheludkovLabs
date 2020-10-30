ac8915fb0cead73cac708229fcaaf9c0f8160dd9
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 23:44:56 +0300

    Scripted auto-commit on change (2020-10-30 23:44:56) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportApp.java
+++ src/main/java/ru/bmstu/lab2/AirportApp.java
@@ -18,5 +18,7 @@ public class AirportApp {
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, AirportMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        
     }
 }
