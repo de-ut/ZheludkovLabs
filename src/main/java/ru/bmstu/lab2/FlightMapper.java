d0870f3d980c2b4f693bfa943bb151744985f619
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 22:47:03 +0300

    Scripted auto-commit on change (2020-10-30 22:47:03) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/lab2/FlightMapper.java
@@ -10,7 +10,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportID, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         String[] fields = value.toString().replace("\"", "").split(",");
-        if(fields[18] )
+        if(!fields[18].isEmpty())
         context.write(new AirportID(fields[0], true), new Text(fields[1]));
     }
 }
