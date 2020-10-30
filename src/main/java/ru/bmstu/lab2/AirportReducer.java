5def2492ba2ebda41fe724e93c31b291f65b1a90
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 23:20:54 +0300

    Scripted auto-commit on change (2020-10-30 23:20:54) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportReducer.java
+++ src/main/java/ru/bmstu/lab2/AirportReducer.java
@@ -3,6 +3,11 @@ package ru.bmstu.lab2;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 public class AirportReducer extends Reducer<AirportID, Text, Text, Text> {
-    
+    @Override
+    protected void reduce(AirportID key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        super.reduce(key, values, context);
+    }
 }
