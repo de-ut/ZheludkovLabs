25d9083a07b133d76bcb8f8a91fa369d22f99051
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 23:23:22 +0300

    Scripted auto-commit on change (2020-10-30 23:23:22) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportReducer.java
+++ src/main/java/ru/bmstu/lab2/AirportReducer.java
@@ -8,9 +8,12 @@ import java.io.IOException;
 public class AirportReducer extends Reducer<AirportID, Text, Text, Text> {
     @Override
     protected void reduce(AirportID key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        
+
         int count = 0;
         float min = Float.MAX_VALUE;
         float max = Float.MIN_VALUE;
         float avg = 0;
+
     }
 }
