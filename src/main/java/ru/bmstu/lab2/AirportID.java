73068724006b1d00f18cc27849b1276c55ccca91
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 16:10:13 +0300

    Scripted auto-commit on change (2020-10-30 16:10:13) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportID.java
+++ src/main/java/ru/bmstu/lab2/AirportID.java
@@ -2,5 +2,23 @@ package ru.bmstu.lab2;
 
 import org.apache.hadoop.io.WritableComparable;
 
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
 public class AirportID implements WritableComparable<AirportID> {
+    @Override
+    public int compareTo(AirportID o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
