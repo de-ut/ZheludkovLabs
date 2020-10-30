bec1fc1642706dd16633897cdb951f03e1ff820d
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 23:09:16 +0300

    Scripted auto-commit on change (2020-10-30 23:09:16) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportGroupingComparator.java
+++ src/main/java/ru/bmstu/lab2/AirportGroupingComparator.java
@@ -3,5 +3,8 @@ package ru.bmstu.lab2;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportGroupingComparator extends WritableComparator {
-    
+    @Override
+    public int compare(Object a, Object b) {
+        return super.compare(a, b);
+    }
 }
