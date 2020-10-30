ad5d4dea840aa5ee9f99717e7e749296f39617a2
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 23:05:03 +0300

    Scripted auto-commit on change (2020-10-30 23:05:03) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportPartitioner.java
+++ src/main/java/ru/bmstu/lab2/AirportPartitioner.java
@@ -1,6 +1,11 @@
 package ru.bmstu.lab2;
 
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Partitioner;
 
 public class AirportPartitioner extends Partitioner<AirportID, Text> {
+    @Override
+    public int getPartition(AirportID airportID, Text text, int i) {
+        return 0;
+    }
 }
