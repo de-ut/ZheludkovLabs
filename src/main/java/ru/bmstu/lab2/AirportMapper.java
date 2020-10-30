6429865a1a2073c593d94af9d3d7987dd0294a1c
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 16:08:19 +0300

    Scripted auto-commit on change (2020-10-30 16:08:19) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/lab2/AirportMapper.java
@@ -3,5 +3,5 @@ package ru.bmstu.lab2;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 
-public class AirportMapper extends Mapper<LongWritable, Text, , Text>{
+public class AirportMapper extends Mapper<LongWritable, Text, AirportID, Text>{
 }
