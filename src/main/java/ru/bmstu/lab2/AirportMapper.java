75f546a3f218e4fa9d71169bd4bd43ebf71ab381
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-10-30 16:29:57 +0300

    Scripted auto-commit on change (2020-10-30 16:29:57) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/AirportMapper.java
+++ src/main/java/ru/bmstu/lab2/AirportMapper.java
@@ -4,7 +4,11 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, AirportID, Text> {
     @Override
-    
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        super.map(key, value, context);
+    }
 }
