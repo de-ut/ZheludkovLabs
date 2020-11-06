0f012d5ed034d56705375467a5bf82d232c2cbe5
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-11-06 12:18:58 +0300

    Scripted auto-commit on change (2020-11-06 12:18:58) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportApp.java
+++ src/main/java/ru/bmstu/lab3/AirportApp.java
@@ -1,5 +1,7 @@
 package ru.bmstu.lab3;
 
+import org.apache.spark.SparkConf;
+
 public class AirportApp {
     public static void main(String[] args) {
         if(args.length != 3){
@@ -8,5 +10,6 @@ public class AirportApp {
         }
 
         SparkConf conf = new SparkConf().setAppName("AirportApp");
+        
     }
 }
