77468da55afa681e2596aef53f8f09a72f411aac
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-11-06 10:40:12 +0300

    Scripted auto-commit on change (2020-11-06 10:40:12) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/FlightMapper.java
+++ src/main/java/ru/bmstu/lab2/FlightMapper.java
@@ -7,8 +7,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 import java.io.IOException;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportID, Text> {
-    private static final String QUOTE_PATTERN = "\"";
-    private static final String COMMA_SEPARATOR = ",";
     private static final int AIRPORT_ID = 14;
     private static final int ARRIVAL_DELAY = 18;
     private static final String FLOAT_ZERO_STRING = "0.00";
