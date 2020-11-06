2cc71081df4f12aaafe5fca8a7d030ad24fd6969
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-11-06 12:50:43 +0300

    Scripted auto-commit on change (2020-11-06 12:50:43) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/FlightData.java
+++ src/main/java/ru/bmstu/lab3/FlightData.java
@@ -3,6 +3,6 @@ package ru.bmstu.lab3;
 import java.io.Serializable;
 
 public class FlightData implements Serializable {
-    private float 
+    private float minDelay;
     public FlightData() {};
 }
