4d00873ef0a63ab7bc674395d0138542a1a3bbdc
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-11-06 10:36:13 +0300

    Scripted auto-commit on change (2020-11-06 10:36:13) by gitwatch.sh

--- src/main/java/ru/bmstu/lab2/Utilities.java
+++ src/main/java/ru/bmstu/lab2/Utilities.java
@@ -4,7 +4,7 @@ public class Utilities {
     private static final String QUOTE_PATTERN = "\"";
     private static final String COMMA_SEPARATOR = ",";
 
-    public String[] separate(String value, int limit){
+    public static String[] separate(String value, int limit){
         return value.toString().replace(QUOTE_PATTERN, "").split(COMMA_SEPARATOR, limit);
     }
 }
