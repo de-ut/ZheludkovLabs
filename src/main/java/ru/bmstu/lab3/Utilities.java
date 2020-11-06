2573821a0ca8038de77d5a2f3dfcb15ce5bd783d
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-11-06 12:29:47 +0300

    Scripted auto-commit on change (2020-11-06 12:29:47) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/Utilities.java
+++ src/main/java/ru/bmstu/lab3/Utilities.java
@@ -1,4 +1,12 @@
-package ru.bmstu.lab3;
+package ru.bmstu.lab2;
 
-public class Utilities {
+import org.apache.hadoop.io.Text;
+
+public final class Utilities {
+    private static final String QUOTE_PATTERN = "\"";
+    private static final String COMMA_SEPARATOR = ",";
+
+    public static String[] separate(String value, int limit){
+        return value.replace(QUOTE_PATTERN, "").split(COMMA_SEPARATOR, limit);
+    }
 }
