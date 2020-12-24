4d0a6482518a39a7ca836154001eceae013a941a
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-12-24 16:58:04 +0300

    Scripted auto-commit on change (2020-12-24 16:58:04) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportApp.java
+++ src/main/java/ru/bmstu/lab3/AirportApp.java
@@ -49,7 +49,7 @@ public class AirportApp {
                 })
                 .collectAsMap();
 
-        final Broadcast<Map<String,String>> air
+        final Broadcast<Map<String,String>> airportBrodcast
 
 
 
