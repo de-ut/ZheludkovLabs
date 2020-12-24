4a81246500a92d34b142e59aa17a1d9d034bccf6
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-12-24 16:59:54 +0300

    Scripted auto-commit on change (2020-12-24 16:59:54) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportApp.java
+++ src/main/java/ru/bmstu/lab3/AirportApp.java
@@ -51,7 +51,7 @@ public class AirportApp {
 
         final Broadcast<Map<String,String>> airportsBroadcasted = sparkContext.broadcast(airports);
 
-        JavaRDD<String> result = flights.map 
+        JavaRDD<String> result = flights.map()
 
 
 
