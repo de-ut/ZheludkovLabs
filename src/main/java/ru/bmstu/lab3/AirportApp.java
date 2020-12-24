1096ab462662fdcf4c4ee442189609fca0ada170
Balabanov Aleksei <mr.aleks756@gmail.com>
2020-12-24 16:31:06 +0300

    Scripted auto-commit on change (2020-12-24 16:31:06) by gitwatch.sh

--- src/main/java/ru/bmstu/lab3/AirportApp.java
+++ src/main/java/ru/bmstu/lab3/AirportApp.java
@@ -8,7 +8,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 public class AirportApp {
-    
+    public static final int FLIGHT_
 
     public static final int FLIGHT_SEPARATION_LIMIT = 0;
     public static final int AIRPORT_SEPARATION_LIMIT = 2;
